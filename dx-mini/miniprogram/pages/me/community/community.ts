cc34c52d58099fc13e2618cb2a217be5d5e244a1
rainson <rainsonma@gmail.com>
2026-04-20 14:42:10 +0800

    feat(mini): custom nav on stub pages with back button

--- dx-mini/miniprogram/pages/me/community/community.ts
+++ dx-mini/miniprogram/pages/me/community/community.ts
@@ -1,1 +1,13 @@
-Page({})
+Page({
+  data: {
+    statusBarHeight: 20,
+  },
+  onLoad() {
+    const sys = wx.getSystemInfoSync()
+    const statusBarHeight = sys.statusBarHeight || 20
+    this.setData({ statusBarHeight })
+  },
+  goBack() {
+    wx.navigateBack()
+  },
+})
