c4bc1b259527b5087801214eb54602eb512490f2
rainson <rainsonma@gmail.com>
2026-05-03 18:37:36 +0800

    refactor(api): renumber user_masters; nullable content_item_id, add content_vocab_id, soft-delete

--- dx-api/database/migrations/20260322000045_create_user_masters_table.go
+++ dx-api/database/migrations/20260322000045_create_user_masters_table.go
@@ -6,26 +6,26 @@ import (
 	"github.com/goravel/framework/facades"
 )
 
-type M20260322000044CreateUserMastersTable struct{}
+type M20260322000045CreateUserMastersTable struct{}
 
-func (r *M20260322000044CreateUserMastersTable) Signature() string {
-	return "20260322000044_create_user_masters_table"
+func (r *M20260322000045CreateUserMastersTable) Signature() string {
+	return "20260322000045_create_user_masters_table"
 }
 
-func (r *M20260322000044CreateUserMastersTable) Up() error {
+func (r *M20260322000045CreateUserMastersTable) Up() error {
 	if !facades.Schema().HasTable("user_masters") {
 		return facades.Schema().Create("user_masters", func(table schema.Blueprint) {
 			table.Uuid("id")
 			table.Primary("id")
 			table.Uuid("user_id")
-			table.Uuid("content_item_id")
+			table.Uuid("content_item_id").Nullable()
+			table.Uuid("content_vocab_id").Nullable()
 			table.Uuid("game_id")
 			table.Uuid("game_level_id")
 			table.TimestampTz("mastered_at").Nullable()
+			table.SoftDeletesTz()
 			table.TimestampsTz()
-			table.Unique("user_id", "content_item_id")
 			table.Index("user_id")
-			table.Index("content_item_id")
 			table.Index("game_id")
 			table.Index("game_level_id")
 			table.Index("created_at")
@@ -34,6 +34,6 @@ func (r *M20260322000044CreateUserMastersTable) Up() error {
 	return nil
 }
 
-func (r *M20260322000044CreateUserMastersTable) Down() error {
+func (r *M20260322000045CreateUserMastersTable) Down() error {
 	return facades.Schema().DropIfExists("user_masters")
 }
